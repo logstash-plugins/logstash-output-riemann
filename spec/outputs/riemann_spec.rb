2b4ff3ccea6decdd76219bb313551b3627c893d3
Jacob Hitze <jhitze@purduefed.com>
2015-06-03 16:53:46 -0400

    Add protocol tests. Fix spacing.
    Fixes #5

--- spec/outputs/riemann_spec.rb
+++ spec/outputs/riemann_spec.rb
@@ -3,10 +3,36 @@ require "logstash/plugin"
 require "logstash/json"
 
 describe "outputs/riemann" do
+
   context "registration" do
+
     it "should register" do
       output = LogStash::Plugin.lookup("output", "riemann").new
-      expect {output.register}.to_not raise_error
+      expect {output.register}.not_to raise_error
+    end
+
+    context "protocol" do
+      it "should fail if not set to [tcp] or [udp]" do
+        expect {
+          output = LogStash::Plugin.lookup("output", "riemann").new("protocol" => "fake")
+          output.register
+          }.to raise_error
+      end
+
+      it "should not error out if set to [tcp]" do
+        expect {
+          output = LogStash::Plugin.lookup("output", "riemann").new("protocol" => "tcp")
+          output.register
+          }.not_to raise_error
+      end
+
+      it "should not error out if set to [udp]" do
+        expect {
+          output = LogStash::Plugin.lookup("output", "riemann").new("protocol" => "udp")
+          output.register
+          }.not_to raise_error
+      end
+      
     end
   end
 end
