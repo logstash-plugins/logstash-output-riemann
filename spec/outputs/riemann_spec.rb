d7d41ae8a1351a7a13f61d814687ff493de100ec
Jacob Hitze <jhitze@purduefed.com>
2015-06-03 16:34:04 -0400

    Basic test. Need the plain codec for this to run.
    Fixes #5

--- spec/outputs/riemann_spec.rb
+++ spec/outputs/riemann_spec.rb
@@ -1,1 +1,12 @@
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/plugin"
+require "logstash/json"
+
+describe "outputs/riemann" do
+  context "registration" do
+    it "should register" do
+      output = LogStash::Plugin.lookup("output", "riemann").new
+      expect {output.register}.to_not raise_error
+    end
+  end
+end
