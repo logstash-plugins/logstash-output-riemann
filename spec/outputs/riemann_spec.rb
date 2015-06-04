da70566c0a788ebc733ed81f48e8b2176bcb1303
Jacob Hitze <jhitze@purduefed.com>
2015-06-03 20:17:45 -0400

    Should be able to push a simple event.

--- spec/outputs/riemann_spec.rb
+++ spec/outputs/riemann_spec.rb
@@ -32,7 +32,18 @@ describe "outputs/riemann" do
           output.register
           }.not_to raise_error
       end
-      
+
+    end
+  end
+
+  context "receive" do
+    output = LogStash::Plugin.lookup("output", "riemann").new
+    data = {"message"=>"hello", "@version"=>"1", "@timestamp"=>"2015-06-03T23:34:54.076Z", "host"=>"vagrant-ubuntu-trusty-64"}
+    event = LogStash::Event.new data
+    it "should accept the event" do
+      expect{
+        output.receive event
+        }.not_to raise_error
     end
   end
 end
